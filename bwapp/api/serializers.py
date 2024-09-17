a001038979311e5b45ba13166b71d75e0730e61c
Vladislav <vdronovvladislav@gmail.com>
2024-09-17 23:32:51 +0300

    Добавлена валидация поля name в сериализаторе

--- bwapp/api/serializers.py
+++ bwapp/api/serializers.py
@@ -6,6 +6,10 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     """Сериализатор модели Product."""
+    name = serializers.CharField(
+        allow_blank=False,
+        error_messages={'blank': 'Название товара не может быть пустым!'}
+    )
     price = serializers.FloatField(
         min_value=0,
         error_messages={'min_value': 'Цена не может быть отрицательной!'}
